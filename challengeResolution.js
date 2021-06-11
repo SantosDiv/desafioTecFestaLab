5ae3cde1f55454f41f45e7e9afa57da0cfa03b41
Diogenes <diogenesfelipe.s@outlook.com>
2021-06-10 21:04:28 -0300

    fix/challengeResolution

--- challengeResolution.js
+++ challengeResolution.js
@@ -2,32 +2,32 @@
 const bugsList = [
   {
     id: 1,
-    title: "Convidados não conseguem confirmar a presença em um evento",
-    age: "1 dia",
-    priority: "Crítico",
+    title: 'Convidados não conseguem confirmar a presença em um evento',
+    age: '1 dia',
+    priority: 'Crítico',
   },
   {
     id: 2,
-    title: "Lista de convidados com cortados na barra vertical",
-    age: "2 dia",
-    priority: "Normal",
+    title: 'Lista de convidados com cortados na barra vertical',
+    age: '2 dia',
+    priority: 'Normal',
   },
   {
     id: 3,
-    title: "Lista de presentes não é visualizada por convidados",
-    age: "2 dia",
-    priority: "Crítico",
+    title: 'Lista de presentes não é visualizada por convidados',
+    age: '2 dia',
+    priority: 'Crítico',
   },
   {
     id: 4,
-    title: "Filtro da categoria `Batizados` não está retornando os convites corretos",
-    age: "3 dia",
-    priority: "Normal",
+    title: 'Filtro da categoria `Batizados` não está retornando os convites corretos',
+    age: '3 dia',
+    priority: 'Normal',
   },
 ];
 
 const listBugIdsToFixToday = (bugsList) => bugsList.reduce((arr, crr) => {
-    if (crr.priority === "Crítico") arr.push(crr.id);
+    if (crr.priority === 'Crítico') arr.push(crr.id);
     return arr;
   }, []);
 
